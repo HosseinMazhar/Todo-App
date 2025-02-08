cb761c783918f7eefd77f70a41da9a8edf975235
hosseinmazhar <hosseinmazhar1053@gmail.com>
2025-02-09 00:59:32 +0330

    react query installed

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,11 +1,16 @@
-"use client"
- 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
- 
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactQueryProvider } from "./ReactQueryProvider";
+
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      <ReactQueryProvider>{children}</ReactQueryProvider>
+    </NextThemesProvider>
+  );
 }
