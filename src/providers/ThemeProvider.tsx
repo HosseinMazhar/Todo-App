dae6d77097c31f05a5a5eb537f812171e0451c27
Hossein Mazhar <hosseinmazhar1053@gmail.com>
2025-02-09 11:05:25 +0330

    add reactquery provider

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -2,15 +2,10 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ReactQueryProvider } from "./ReactQueryProvider";
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return (
-    <NextThemesProvider {...props}>
-      <ReactQueryProvider>{children}</ReactQueryProvider>
-    </NextThemesProvider>
-  );
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
